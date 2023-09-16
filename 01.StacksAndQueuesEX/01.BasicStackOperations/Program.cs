89a70006385dbd68683d2d1c0e166f15614f876e
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-09-16 22:25:41 +0300

    task completed

---
+++ 01.StacksAndQueuesEX/01.BasicStackOperations/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int elementsToPush = commands[0];
+            int elementsToPop = commands[1];
+            int elementToPeek = commands[2];
+
+            int[] numbers = new int[elementsToPush];
+
+            numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            while (stack.Any() && elementsToPop > 0)
+            {
+                stack.Pop();
+                elementsToPop--;
+            }
+            if (!stack.Any())
+            {
+                Console.WriteLine(0);
+            }
+            else if (stack.Contains(elementToPeek))
+            {
+                Console.WriteLine("true");
+            }
+            else if (!stack.Contains(elementToPeek))
+            {
+                Console.WriteLine(stack.Min());
+            }
+
+            
+        }
+    }
+}
