7a6f3acf4438b0d732fae4d092c59119c62d4b09
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-09-16 21:59:16 +0300

    solution

---
+++ 01.StacksAndQueuesLab/01.ReverseAString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<string> stack = new Stack<string>();
+
+            foreach (char letter in input)
+            {
+                stack.Push(letter.ToString());
+            }
+            while (stack.Any())
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
