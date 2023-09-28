e410648aaca5b7ca4b965bc1c4e9ad26118a1cda
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-09-28 17:57:32 +0300

    solution

---
+++ 04.Streams, Files and DirectoriesEX/01.EvenLines/EvenLines.cs
@@ -0,0 +1,43 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            string text = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            using (StreamReader readText = new StreamReader(inputFilePath))
+            {
+                int lineCount = 0;
+
+                while (!readText.EndOfStream)
+                {
+                    text = readText.ReadLine();
+                    if (lineCount % 2 == 0)
+                    {
+                        text = text.Replace('.', '@').Replace(',', '@').Replace('!', '@').Replace('?', '@').Replace('-', '@');
+                        string[] textAsArr = text.Split();
+                        Array.Reverse(textAsArr);
+                        text = string.Join(" ", textAsArr);
+                        sb.Append(text + "\n");
+                    }
+
+                    lineCount++;
+                }
+
+            }
+            return sb.ToString();
+        }
+    }
+}
