6ae6fa4269e4f570abae2af4b95eabcd8871c29c
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-09-29 14:53:53 +0300

    solution

---
+++ 04.Streams, Files and DirectoriesEX/03.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,36 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            
+            using FileStream fileToRead = new FileStream(inputFilePath, FileMode.Open);
+            
+
+            using (FileStream fileToWrite = new FileStream(outputFilePath, FileMode.Create))
+            {
+                int readCount;
+                byte[] buffer = new byte[1024];
+                while ((readCount = fileToRead.Read(buffer, 0, buffer.Length)) != 0)
+                {                   
+                    
+                    fileToWrite.Write(buffer, 0, buffer.Length);
+                }
+                              
+            }
+           
+        }
+    }
+}
---
+++ 04.Streams, Files and DirectoriesEX/04.DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,59 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            SortedDictionary<string, List<FileInfo>> extensionsFiles = new SortedDictionary<string, List<FileInfo>>();
+            string[] files = Directory.GetFiles(inputFolderPath);
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+
+                if (!extensionsFiles.ContainsKey(fileInfo.Extension))
+                {
+                    extensionsFiles.Add(fileInfo.Extension, new List<FileInfo>());
+                }
+                extensionsFiles[fileInfo.Extension].Add(fileInfo);
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var exFile in extensionsFiles.OrderByDescending(ef => ef.Value.Count))
+            {
+                stringBuilder.AppendLine(exFile.Key);
+
+                foreach (var file in exFile.Value.OrderBy(f => f.Length))
+                {
+                    stringBuilder.AppendLine($"--{file.Name} - {(double)file.Length / 1024 :f3}kb");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+
+            File.WriteAllText(filePath, textContent);
+        }
+    }
+}
