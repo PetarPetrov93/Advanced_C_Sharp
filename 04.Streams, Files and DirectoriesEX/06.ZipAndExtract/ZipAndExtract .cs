f44c4d2c57745b9f70d69b4e4ffb0268e7ec064f
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-09-29 15:34:00 +0300

    solution

---
+++ 04.Streams, Files and DirectoriesEX/06.ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,33 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            
+            ZipFile.CreateFromDirectory(inputFilePath, zipArchiveFilePath);
+            File.Copy(inputFilePath, zipArchiveFilePath);
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            ZipFile.ExtractToDirectory(zipArchiveFilePath, outputFilePath);
+        }
+    }
+}
