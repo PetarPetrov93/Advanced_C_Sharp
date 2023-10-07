2c2930c6c10ee4c9801c9bf823a7b767dfce512e
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-10-07 16:48:15 +0300

    solutions

---
+++ 09.Generics/09.GenericsEX/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T input;
+        public Box(T input) 
+        {
+            this.input = input;
+            
+        }
+
+        public override string ToString()
+        {
+            
+            return input.GetType().FullName +": " + input;
+        } 
+
+    }
+}
