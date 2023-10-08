2135a1920f1894b27347ac3b2d5995c856c305ed
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-10-08 16:51:29 +0300

    solution

---
+++ 09.Generics/09.GenericsEX/08.Threeuple/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _08.Threeuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personAndAddress = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string fullName = personAndAddress[0] + " " + personAndAddress[1];
+            string address = personAndAddress[2];
+            string[] town = personAndAddress.Skip(3).ToArray();
+            string townName = string.Join(" ", town);
+             
+
+            Threeuple<string, string, string> threeuple1 = new Threeuple<string, string, string>(fullName, address, townName);
+
+            Console.WriteLine(threeuple1.Item1 + " -> " + threeuple1.Item2 + " -> " + threeuple1.Item3);
+
+
+            string[] personAndBeer = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = personAndBeer[0];
+            int beers = int.Parse(personAndBeer[1]);
+            string drunkOrNot = personAndBeer[2];
+            bool isDrunk = false;
+            if (drunkOrNot == "drunk")
+            {
+                isDrunk = true;
+            }
+
+            Threeuple<string, int, bool> threeuple2 = new Threeuple<string, int, bool>(name, beers, isDrunk);
+
+            Console.WriteLine(threeuple2.Item1 + " -> " + threeuple2.Item2 + " -> " + threeuple2.Item3);
+
+
+            string[] nameBalanceBank = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string accHolder = nameBalanceBank[0];
+            double balance = double.Parse(nameBalanceBank[1]);
+            string bank = nameBalanceBank[2];
+
+            Threeuple<string, double, string> threeuple3 = new Threeuple<string, double, string>(accHolder, balance, bank);
+
+            Console.WriteLine(threeuple3.Item1 + " -> " + threeuple3.Item2 + " -> " + threeuple3.Item3);
+        }
+    }
+}
---
+++ 09.Generics/09.GenericsEX/08.Threeuple/Threeuple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        private T1 item1;
+        private T2 item2;
+        private T3 item3;
+
+        public Threeuple(T1 item1, T2 item2, T3 item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+
+        public T1 Item1
+        {
+            get { return item1; }
+            set { item1 = value; }
+        }
+        public T2 Item2
+        {
+            get { return item2; }
+            set { item2 = value; }
+        }
+        public T3 Item3
+        {
+            get { return item3; }
+            set { item3 = value; }
+        }
+    }
+}
