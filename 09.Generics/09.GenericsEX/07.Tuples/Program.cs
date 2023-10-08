b30e65369772f38485db334ba9e08a38c62e78f8
PetarPetrov93 <petarpetrov_93@abv.bg>
2023-10-08 16:34:17 +0300

    solution

---
+++ 09.Generics/09.GenericsEX/07.Tuples/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _07.Tuples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string[] personAndAddress = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string fullName = personAndAddress[0] + " " + personAndAddress[1];
+            string address = personAndAddress[2];
+            Tuple<string, string> tuple1 = new Tuple<string, string>(fullName, address);
+
+            Console.WriteLine(tuple1.Item1 + " -> " + tuple1.Item2);
+
+
+            string[] personAndBeer = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = personAndBeer[0];
+            int beers = int.Parse(personAndBeer[1]);
+            Tuple<string, int> tuple2 = new Tuple<string, int>(name, beers);
+
+            Console.WriteLine(tuple2.Item1 + " -> " + tuple2.Item2);
+
+
+            string[] intAndDouble = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int num1 = int.Parse(intAndDouble[0]);
+            double num2 = double.Parse(intAndDouble[1]);
+            Tuple<int, double> tuple3 = new Tuple<int, double>(num1, num2);
+
+            Console.WriteLine(tuple3.Item1 + " -> " + tuple3.Item2);
+
+        }
+    }
+}
